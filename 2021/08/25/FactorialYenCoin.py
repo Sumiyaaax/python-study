508ed432feae7b8f8b4e00e1ec5ba09a06f7beab
Sumiya Kurihara <wave_soul14@yahoo.co.jp>
2021-08-25 08:50:20 +0900

    20210825途中

---
+++ 2021/08/25/FactorialYenCoin.py
@@ -0,0 +1,19 @@
+P = int(input())
+def getFactorial(i):
+    tmp = 1
+    for j in range(1, i):
+        tmp *= j
+    return tmp
+
+factorialList = list()
+for i in range(1, 11):
+    factorialList.append(getFactorial(i))
+total = 0
+answer = 0
+for i in reversed(range(1, 11)):
+    while factorialList[i - 1] <= P - total:
+        total += factorialList[i - 1]
+        answer += 1
+
+print(answer)
+
