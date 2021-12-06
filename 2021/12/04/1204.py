b3cbd5cd6706002172dc7b481f05bce68c478e8d
Sumiya Kurihara <wave_soul14@yahoo.co.jp>
2021-12-07 08:25:31 +0900

    20211207

--- 2021/12/04/1204.py
+++ 2021/12/04/1204.py
@@ -1,1 +1,8 @@
-# vueでcommitしたけど反映されないのでこちら
+import pandas as pd
+
+CSV_FILE='covid-19_daily_survey.csv'
+df = pd.read_csv(CSV_FILE)
+print(df)
+print(len(df))
+print(len(df.columns))
+print(df.info())
