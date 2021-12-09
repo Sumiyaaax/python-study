f62fe0c395509629c8edb3d62bec3078839d26bd
Sumiya Kurihara <wave_soul14@yahoo.co.jp>
2021-12-10 08:28:25 +0900

    20211210

--- 2021/12/04/1204.py
+++ 2021/12/04/1204.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import os
 
 CSV_FILE='covid-19_daily_survey.csv'
+OUTPUT_CSV_FILE='output_2.csv'
 df = pd.read_csv(CSV_FILE)
 print(df)
 print(len(df))
@@ -9,5 +11,9 @@ print(df.info())
 cols = df.columns.values.tolist()
 print(df.iloc[:2])
 print(cols)
-df = df[['医療機関ID','医療機関名']]
+df = df[['医療機関ID','医療機関名', '都道府県名', '医療機関住所', '電話番号']]
 print(df)
+df.to_csv(OUTPUT_CSV_FILE, index=False)
+print(os.listdir('./'))
+os.remove('output_1.csv')
+print(os.listdir('./'))
