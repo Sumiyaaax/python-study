5f868fe45f3b9fb6be65332f9453ee2b0510414f
Sumiya Kurihara <wave_soul14@yahoo.co.jp>
2021-12-23 08:33:51 +0900

    20211224

---
+++ 2021/12/23/1223.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import openpyxl
+INPUT_FILE_NAME = 'population2021.xlsx'
+INPUT_BOOK = pd.ExcelFile(INPUT_FILE_NAME)
+INPUT_SHEET_NAME = INPUT_BOOK.sheet_names
+print(INPUT_SHEET_NAME)
