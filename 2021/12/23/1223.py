d89dce4dcbe040ffb89293523d593201f6221e66
Sumiya Kurihara <wave_soul14@yahoo.co.jp>
2021-12-24 08:15:55 +0900

    20211224

--- 2021/12/23/1223.py
+++ 2021/12/23/1223.py
@@ -4,3 +4,5 @@ INPUT_FILE_NAME = 'population2021.xlsx'
 INPUT_BOOK = pd.ExcelFile(INPUT_FILE_NAME)
 INPUT_SHEET_NAME = INPUT_BOOK.sheet_names
 print(INPUT_SHEET_NAME)
+df = INPUT_BOOK.parse(INPUT_SHEET_NAME[0])
+print(df)
